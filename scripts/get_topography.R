9f57ab3848626fe682fc3e50120f112555645d9a
Eryops1 <tietje.melanie@gmail.com>
2020-07-08 14:12:18 +0200

    updates, fixed result file name

--- scripts/get_topography.R
+++ scripts/get_topography.R
@@ -14,24 +14,50 @@ library(vegan)
 library(rgdal)
 
 shape <- readOGR("shapefile/level3.shp")
-elev <- raster("data/worldclim/wc2.1_30s_elev.tif")
+elev <- raster("data/wc2.1_30s_elev.tif")
 
 
 Sys.time()
 elev_mean <- c()
 elev_sd <- c()
-even_elev <- c()
+elev_n <- c()
 for(i in 1:nrow(shape@data)){ #nrow(shape@data)
   shape_sub <- subset(shape, shape$LEVEL_3_CO==shape$LEVEL_3_CO[[i]])
-  rest <- extract(soil, shape_sub)
+  rest <- extract(elev, shape_sub)
   rest <- na.omit(rest[[1]])
-  number_soils <- c(number_soils, length(unique(rest)))
-  simp_soil <- c(simp_soil, diversity(table(rest), index="simp"))
-  even_soil <- c(even_soil, diversity(table(rest))/log(length(unique(rest))))
+  elev_mean <- c(elev_mean, mean(rest))
+  elev_sd <- c(elev_sd, sd(rest))
+  elev_n <- c(elev_n, length(rest))
   if(!i%%1)cat(i,"\r")
 }
 Sys.time()
 
-res <- data.frame(country=shape$LEVEL_3_CO, number_soils, simp_soil, even_soil)
+res <- data.frame(country=shape$LEVEL_3_CO, elev_mean, elev_sd, elev_n)
 
-saveRDS(res, file="soil.rds")
+saveRDS(res, file="topography.rds")
+
+
+
+##### RESULTS ############################################################
+
+# run on server 5h
+res <- readRDS("data/topography.rds")
+
+
+# check
+# Transform to data frame for ggplot
+library(sf)
+library(ggplot2)
+shape$elev_mean <- res$elev_mean
+shape$elev_sd <- res$elev_sd
+shape$elev_n <- res$elev_n
+shp <- st_as_sf(shape)
+
+
+# SOIL MAPS ##########################################################################
+ggplot(shp) +
+  geom_sf(aes(fill = elev_n), lwd=0.1)+
+  theme_void()+
+  scale_fill_continuous(trans="sqrt")
+
+
