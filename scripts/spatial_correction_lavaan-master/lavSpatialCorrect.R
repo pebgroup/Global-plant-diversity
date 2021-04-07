48cb1246ca352362a4265aaf8313ed01f61e14f1
Eryops1 <tietje.melanie@gmail.com>
2021-04-07 15:51:34 +0200

    spatial autocorrelation anaylsis has separate Rmd now. finished SAC part paleoclimate and DR residuals check

--- scripts/spatial_correction_lavaan-master/lavSpatialCorrect.R
+++ scripts/spatial_correction_lavaan-master/lavSpatialCorrect.R
@@ -128,6 +128,81 @@ lavSpatialCorrect_correct <- function(obj, xvar, yvar, alpha=0.05){
 
 #lavSpatialCorrect(obj, xvar, yvar)
 
+lavSpatialCorrect_moran.mc <- function(obj, xvar, yvar, alpha=0.05){
+  require(lavaan)
+  require(ape)
+
+  #  TEST DATA
+  obj=c2.fit.p
+  xvar=dat_no.na$x
+  yvar=dat_no.na$y
+  distance=1600
+  alpha=0.05
+  distance=c(500,2000)
+  
+  #first, get the residuals from the model
+  if(length(lavNames(obj, type="lv"))!=0){
+    resids <- as.data.frame(residuals(obj, "casewise"))
+  }else{
+    resids <- as.data.frame(my.residuals_lavaan(obj))
+  }
+  
+  #get only endogenous variables
+  resids <- resids[,which(apply(resids, 2, function(x) length(unique(x))) !=1)]
+  
+  
+  #make a distance matrix
+  distMat <- as.matrix(dist(cbind(xvar, yvar)))
+  
+  #invert this matrix for weights
+  distsInv <- 1/distMat
+  diag(distsInv) <- 0
+  
+  morans_i <- lapply(resids,  function(x){
+    mi <- moran.mc(c(x), mat2listw(distsInv, style = "B"), nsim=599, zero.policy=T)
+    #mi <- Moran.I(c(x), distsInv)
+    if(mi$p.value>alpha){
+      mi$n.eff <- nrow(resids) #don't correct sample size
+    }else{
+      #large sample size approximation
+      mi$n.eff <- nrow(resids)*(1-mi$statistic)/(1+mi$statistic)
+    }
+    
+    #return the list
+    mi
+    
+  })
+  
+  #get the vcov matrix
+  v <- diag(vcov(obj))
+  n <- nrow(resids)
+  #using new sample sizes, for each variable, calculate new Z-scores
+  params <- lapply(names(morans_i), function(acol){
+    idx <- grep(paste0(acol, "~"),names(v))  #regression or covariances
+    idx <- c(idx, grep(paste0("=~",acol),names(v)))  #latent variable definitions
+    v_idx <- v[idx]*n/morans_i[[acol]]$n.eff
+    ret <-  data.frame(Parameter = names(v)[idx], Estimate=coef(obj)[idx], 
+                       n.eff = morans_i[[acol]]$n.eff, Std.err = sqrt(v_idx))
+    ret[["Z-value"]] <- ret$Estimate/ret$Std.err
+    ret[["P(>|z|)"]] <- 2*pnorm(abs(ret[["Z-value"]]), lower.tail=F)
+    
+    ret
+    
+  })
+  names(params) <- names(morans_i)
+  
+  mi <- lapply(morans_i, function(m) {
+    data.frame(observed=m$observed, expected=m$expected, 
+               sd=m$sd, p.value = m$p.value, n.eff = m$n.eff)
+  })
+  
+  return(list(Morans_I = mi, parameters=params)) 
+  
+}
+# does not work yet
+
+
+
 
 
 my.lavSpatialCorrect <- function(obj, xvar, yvar, distance, alpha=0.05){
