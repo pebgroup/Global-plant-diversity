7851ab0fbd23d71b824dc3aab4f2ad8bbefc0541
Eryops1 <tietje.melanie@gmail.com>
2021-05-18 15:27:16 +0200

    corrected path names to final folder structure in master script, all other new folder scripts

--- master_analysis.R
+++ master_analysis.R
@@ -4,14 +4,17 @@
 # 
 # This script sources all analysis scripts. 
 # 
-# Please note that the most time consuming calculations are muted by the following 
-# and instead the results are loaded where needed. This affects scripts to
-# taxonomically add species to the phylogeny, resolve polytomies, extract climate
-# variables and run automated SEM selection procedure. Several analyses are
-# set up for parallel processing (up to 16 cores) to speed up performance.
+# Please note that long runtime steps are muted by default 
+# and instead the results of these steps loaded where needed. This affects 
+# scripts to taxonomically add species to the phylogeny, resolve polytomies, 
+# extract climate variables and run automated SEM selection procedure. 
+# Several analyses are set up for parallel processing (16 cores) 
+# to speed up performance.
 # 
-# Processing partly large data files, tested on a linux system (Ubuntu 20.04) with 
-# Intel© Core™ i7-8565U CPU @ 1.80GHz × 4 , 16GB RAM, RStudio Version 1.3.1073
+# Processing partly large data files, tested on Ubuntu 20.04 with 
+# Intel© Core™ i7-8565U CPU @ 1.80GHz × 4 , 16GB RAM, R version 3.6.3, 
+# RStudio Version 1.3.1073
+#
 # All used R packages are cited in the supplement
 
 
@@ -20,11 +23,12 @@ run_p = FALSE
 run_s=FALSE
 run_e=FALSE
 run_ms=FALSE
+ncores = 16
 
-dir.create("publish_figures")
+dir.create("figures")
 dir.create("processed_data")
 
-source("publish_scripts/functions.R")
+source("scripts/functions.R")
 
 library(ape)
 library(caret)
@@ -71,7 +75,7 @@ library(writexl) # to export model results as table
 rm(list = setdiff(ls(), c(lsf.str(), "run_p", "run_s", "run_e", "run_ms"))) 
 
 
-wcp <- fread("data/wcs_jun_20/world_checklist_names_and_distribution_jun_2020/checklist_names.txt", quote="")
+wcp <- fread("data/WCVP/checklist_names.txt", quote="")
 saveRDS(wcp, "processed_data/wcp_jun_20.rds")
 
 
@@ -80,13 +84,13 @@ saveRDS(wcp, "processed_data/wcp_jun_20.rds")
 db <- "WCP" 
 data_folder_path <- "./processed_data/"
 WCP_input_filename <- "wcp_jun_20.rds"
-source("publish_scripts/APG_family_lookup.R")
+source("scripts/APG_family_lookup.R")
 
 
 
 # Clean WCP --------------------------------------------------------------------------------------
 wcp <- readRDS("processed_data/apg_wcp_jun_20.rds")
-source("publish_scripts/WCP_cleanup.R")
+source("scripts/WCP_cleanup.R")
 saveRDS(wcp2, "processed_data/apg_wcp_jun_20_clean.rds")
 
 
@@ -104,10 +108,10 @@ saveRDS(wcp2, "processed_data/apg_wcp_jun_20_clean.rds")
 rm(list = setdiff(ls(), c(lsf.str(), "run_p", "run_s", "run_e", "run_ms"))) 
 tax_level <- "elevated_to_species_id" 
 
-phylo <- read.tree("trees/ALLMB.tre") # phylogeny
+phylo <- read.tree("data/ALLMB.tre") # phylogeny from Smith&Brown 2018
 ott <- readRDS("data/ott.rds") # OTL taxonomy to identify tip labels
 matches <- readRDS("data/fin_species_match_NCBI.rds") # NCBI-WCSP matches from the taxonomy matcher
-source("publish_scripts/match_data.R")
+source("scripts/match_data.R")
 # export tip labels that have not been matched with NCBI
 saveRDS(fin[!is.na(fin$gbif_id),], file="processed_data/SB_tip_labels.rds")
 saveRDS(fin, file="processed_data/fin.rds")
@@ -118,7 +122,9 @@ saveRDS(fin, file="processed_data/fin.rds")
 rm(list = setdiff(ls(), c(lsf.str(), "run_p", "run_s", "run_e", "run_ms"))) 
 # Load Smith & Brown tip labels not covered by NCBI 
 sb <- readRDS("processed_data/SB_tip_labels.rds")
-source("publish_scripts/common_format_creator_SB.R")
+# load gbif download
+gbif <- readRDS("data/gbif_all.rds")
+source("scripts/common_format_creator_SB.R")
 saveRDS(input, "processed_data/input_tip_labels.rds")
 
 
@@ -127,7 +133,7 @@ saveRDS(input, "processed_data/input_tip_labels.rds")
 db <- "GBIF" 
 data_folder_path <- "./processed_data/"
 gbif_input_filename <- "input_tip_labels.rds"
-source("publish_scripts/APG_family_lookup.R")
+source("scripts/APG_family_lookup.R")
 # writes apg_input_tip_labels.rds
 
 
@@ -144,7 +150,7 @@ gbif_input_filename <- "apg_input_tip_labels.rds"
 fin_file_species <- paste0("fin_species_match_", DB.name, ".rds")
 bry <- read.csv("data/bryophyta.csv")
 
-source("publish_scripts/taxonomic_matcher.v.1.4.R")
+source("scripts/taxonomic_matcher.v.1.4.R")
 # output: fin_species_match_GBIF.rds
 
 
@@ -152,22 +158,25 @@ source("publish_scripts/taxonomic_matcher.v.1.4.R")
 rm(list = setdiff(ls(), c(lsf.str(), "run_p", "run_s", "run_e", "run_ms"))) 
 matches <- readRDS("processed_data/fin_species_match_GBIF.rds")
 fin <- readRDS("processed_data/fin.rds")
-phylo <- read.tree("trees/ALLMB.tre") # phylogeny
+phylo <- read.tree("data/ALLMB.tre") # phylogeny
 tax_level <- "elevated_to_species_id" 
-phylogb_a <- read.tree("trees/GBMB.tre") # genbank tree to identify tips with molecular data behind them
-source("publish_scripts/match_data_part2.R")
+phylogb_a <- read.tree("data/GBMB.tre") # genbank tree to identify tips with molecular data behind them
+source("scripts/match_data_part2.R")
 write.tree(clean_tree, "processed_data/allmb_matched_clean.tre")
 
 
 
 # Add missing species taxonomically --------------------------------------------------------------
+
+## LONG RUNTIME ! ca 11 hours
+
 rm(list = setdiff(ls(), c(lsf.str(), "run_p", "run_s", "run_e", "run_ms"))) 
 phylo <- read.tree("processed_data/allmb_matched_clean.tre")
 wcp <- readRDS("processed_data/apg_wcp_jun_20_clean.rds")
 ferns <- as.vector(read.csv("data/fern_list.txt")[,1])
 
 if(run_s){
-  source("publish_scripts/add_species_server_version.R", print.eval=T)
+  source("scripts/add_species_server_version.R", print.eval=T)
 }else{
   fin_tree <- read.tree("processed_data/allmb_matched_added_species_clean.tre") # phylogeny
 }
@@ -175,15 +184,16 @@ if(run_s){
 
 
 # Resolve polytomies and get root distance for each tip ------------------------------------------
-## we recommend running this in parallel on a machine that can handle at least 16 cores
-## 16 cores takes about 1h computation time
+
+## LONG RUNTIME ! (ca 1h on 16 cores)
+## PARALLEL PROCESSING
 
 rm(list = setdiff(ls(), c(lsf.str(), "run_p", "run_s", "run_e", "run_ms"))) 
 
 if(run_p){
-  n_cores =16
+  n_cores =ncores
   reps <- 1000
-  source("publish_scripts/resolve_polytomies.R", print.eval = TRUE)
+  source("scripts/resolve_polytomies.R", print.eval = TRUE)
   saveRDS(res, "processed_data/polytomie_RD_results.rds")
 }
 
@@ -199,8 +209,8 @@ rm(list = setdiff(ls(), c(lsf.str(), "run_p", "run_s", "run_e", "run_ms")))
 
 phylo <- read.tree("processed_data/allmb_matched_added_species_clean.tre") 
 wcp <- readRDS("processed_data/apg_wcp_jun_20_clean.rds")
-shape = readOGR(dsn = "shapefile", layer = "level3")
-dist <- fread("data/wcs_jun_20/world_checklist_names_and_distribution_jun_2020/checklist_distribution.txt")
+shape <- readOGR("data/shapefile_bot_countries/level3.shp")
+dist <- fread("data/WCVP/checklist_distribution.txt")
 source(process_geography.R, print.eval = TRUE)
 saveRDS(comm, file="processed_data/comm_April2021.rds")
 
@@ -217,7 +227,7 @@ comm <- readRDS("processed_data/comm_April2021.rds")
 table(colnames(comm) %in% phylo$tip.label) # should be all represented
 
 n_cores=4
-source("publish_scripts/phylostruct.R", print.eval = TRUE)
+source("scripts/phylostruct.R", print.eval = TRUE)
 saveRDS(mrd.res, "processed_data/mrd_Apr2021.rds")
 
 
@@ -226,7 +236,9 @@ saveRDS(mrd.res, "processed_data/mrd_Apr2021.rds")
 
 
 # Environmental variables ##########################################################################
-# running all scripts takes about 6 hours
+
+## LONG RUNTIME ! ca 6 hours
+
 if(run_e){
   rm(list = setdiff(ls(), c(lsf.str(), "run_p", "run_s", "run_e", "run_ms"))) 
   #library(cruts)
@@ -234,26 +246,26 @@ if(run_e){
   # raster files are loaded inside the scripts
   dir.create("processed_data/cru")
   
-  source("publish_scripts/get_climate.R")
+  source("scripts/get_climate.R")
   saveRDS(res, "processed_data/climate.rds")
   
   rm(list = setdiff(ls(), c(lsf.str(), "run_p", "run_s", "run_e", "run_ms"))) 
-  shape <- readOGR("shapefile/level3_mod.shp")
-  soil <- raster("data/soilgrids/soil_raster_layer_000832.tif")
-  source("publish_scripts/get_soil.R")
+  shape <- readOGR("data/shapefile_bot_countries/level3_mod.shp")
+  soil <- raster("data/soil_raster_layer_000832.tif")
+  source("scripts/get_soil.R")
   saveRDS(res, file="processed_data/soil.rds")
   
   rm(list = setdiff(ls(), c(lsf.str(), "run_p", "run_s", "run_e", "run_ms"))) 
-  shape <- readOGR("shapefile/level3_mod.shp")
-  elev <- raster("data/worldclim/wc2.1_30s_elev.tif")
-  source("publish_scripts/get_topography.R")
+  shape <- readOGR("data/shapefile_bot_countries/level3_mod.shp")
+  elev <- raster("data/wc2.1_30s_elev.tif")
+  source("scripts/get_topography.R")
   saveRDS(res, file="processed_data/topography.rds")
   
   rm(list = setdiff(ls(), c(lsf.str(), "run_p", "run_s", "run_e", "run_ms"))) 
 
-  shape <- readOGR("shapefile/level3.shp")
-  olson <- readOGR("shapefile/olson/wwf_terr_ecos.shp")
-  source("publish_scripts/get_biomes.R")
+  shape <- readOGR("data/shapefile_bot_countries/level3.shp")
+  olson <- readOGR("data/shapefile_biomes/wwf_terr_ecos.shp")
+  source("scripts/get_biomes.R")
   saveRDS(res.df, file="processed_data/biomes_olson.rds")
   
 }
@@ -265,13 +277,13 @@ rm(list = setdiff(ls(), c(lsf.str(), "run_p", "run_s", "run_e", "run_ms")))
 
 sr <- readRDS("processed_data/comm_April2021.rds")
 mrd <- readRDS("processed_data/mrd_Apr2021.rds")
-shape <- readOGR("shapefile/level3.shp")
+shape <- readOGR("shapefile_bot_countries/level3.shp")
 soil <- readRDS("processed_data/soil.rds")
 climate <- readRDS("processed_data/climate.rds")
 topography <- readRDS("processed_data/topography.rds")
 biomes <- readRDS("processed_data/biomes_olson.rds")
 
-source("publish_scripts/assemble_dataset.R")
+source("scripts/assemble_dataset.R")
 saveRDS(shp, "processed_data/shp_object_fin_analysis.RDS")
 
 
@@ -281,7 +293,7 @@ saveRDS(shp, "processed_data/shp_object_fin_analysis.RDS")
 rm(list = setdiff(ls(), c(lsf.str(), "run_p", "run_s", "run_e", "run_ms"))) 
 
 shp <- readRDS("processed_data/shp_object_fin_analysis.RDS")
-source("publish_scripts/data_prep_and_checks.R")
+source("scripts/data_prep_and_checks.R")
 # saves internally the final dataset with only complete cases: sem_input_data.rds
 
 
@@ -291,6 +303,10 @@ source("publish_scripts/data_prep_and_checks.R")
 # STRUCTURAL EQUATION MODELING ##################################################################
 
 # SEM model selection ----------------------------------------------------------------------------
+
+## LONG RUNTIME ! ca 5 hours on 16 cores
+## PARALLEL PROCESSING
+
 if(run_ms){
   rm(list = setdiff(ls(), c(lsf.str(), "run_p", "run_s", "run_e", "run_ms"))) 
 
@@ -298,10 +314,10 @@ if(run_ms){
   
   # set max number of variables (to add to the fixed ones) used and register cores
   var.max <- 10
-  registerDoParallel(16)
+  registerDoParallel(ncores)
   
-  source("publish_scripts/mod_selection_SEM.R")
-  save(temp, file="mod_selection_Apr2021.RData")
+  source("scripts/mod_selection_SEM.R")
+  save(temp, file="processed_data/mod_selection_Apr2021.RData")
 }
 
 
@@ -311,7 +327,7 @@ rm(list = setdiff(ls(), c(lsf.str(), "run_p", "run_s", "run_e", "run_ms")))
 
 load("processed_data/mod_selection_Apr2021.RData")
 dat_no.na <- readRDS("processed_data/sem_input_data.rds")
-source("publish_scripts/mod_selection_SEM_analysis.R", print.eval = TRUE)
+source("scripts/mod_selection_SEM_analysis.R", print.eval = TRUE)
 
 save.image("processed_data/all_models.RData")
 save(max.var.mod_mod7, max.var.mod.fit.mod7, file="processed_data/best_model.RData")
@@ -327,9 +343,17 @@ rm(list = setdiff(ls(), c(lsf.str(), "run_p", "run_s", "run_e", "run_ms")))
 load("processed_data/best_model.RData")
 shp <- readRDS("processed_data/shp_object_fin_analysis.RDS") # load spatial object
 dat_no.na <- readRDS("processed_data/sem_input_data.rds")
-#shape <- readOGR("shapefile/level3.shp")
+#shape <- readOGR("shapefile_bot_countries/level3.shp")
 
-source("publish_scripts/sem_results_further_analysis.R")
+source("scripts/sem_results_further_analysis.R")
+
+
+
+
+
+
+
+
 
 
 # FIN #####
---
+++ scripts/APG_family_lookup.R
@@ -0,0 +1,254 @@
+library("tidyverse")
+library("stringr")
+
+#### SETUP #########
+
+# Choose database, possible values: WCP, NCBI, BIEN, GBIF 
+# You can chose more than 1 (e.g. c("BIEN", "NCBI", "GBIF")) 
+
+data_folder_path <- "./processed_data/" # depends on where your working directory is set
+
+# if using BIEN, remove mosses and moss allis
+bryophyta <- "bryophyta.csv"
+
+
+
+# APG edits ###################################################################################
+f.apg <- read.csv("data/apgweb_parsed.csv", stringsAsFactors = F)
+#grep("near_|fossil_", f.apg$Clade)
+
+# remove some clade names don't have a valida name ("near_XXX") or a fossil name ("fossil")
+
+f.apg <- f.apg %>% 
+  filter(!(str_detect(Clade, "near_") | str_detect(Clade, "fossil"))) %>% 
+  select(Syn_Fam, Acc_Fam, Clade)
+
+# Intresting thing: "Adoxaceae" is treated as "syn" name of "Viburnaceae" in APWeb, which is conflict with APG IV; need to swap
+
+# > f.apg[grep("Adoxaceae", f.apg$Syn_Fam),]
+# Syn_Fam     Acc_Fam      Clade
+# 25 Adoxaceae Viburnaceae Dipsacales
+f.apg[grep("Adoxaceae", f.apg$Syn_Fam),]$Acc_Fam <- "Adoxaceae"
+f.apg[grep("Viburnaceae", f.apg$Syn_Fam),]$Acc_Fam <- "Adoxaceae"
+
+# also one more family names "Rhipogonaceae" was not recorded in APWeb
+# so Manual added in "Rhipogonaceae" == "Ripogonaceae"  
+# manually adding Osmundaceae as its accepted according to http://powo.science.kew.org/taxon/urn:lsid:ipni.org:names:77126775-1
+
+f.apg <- rbind.data.frame(f.apg, c("Rhipogonaceae", "Ripogonaceae", "Liliales"),
+                          c("Ripogonaceae", "Ripogonaceae", "Liliales"),
+                          c("Osmundaceae", "Osmundaceae", "")) %>%  
+  arrange(Syn_Fam) %>% 
+  unique()
+
+# remove special character from Isoetaceae 
+f.apg$Syn_Fam <- gsub("Isoëtaceae", "Isoetaceae", f.apg$Syn_Fam)
+f.apg$Acc_Fam <- gsub("Isoëtaceae", "Isoetaceae", f.apg$Acc_Fam)
+
+# caution serval invalid strings in fern clade names (check before do)
+# grep("_", f.apg$Clade)
+#f.apg <- f.apg %>% filter(!(str_detect(Clade, "_"))) # uncomment this line if want to remove ferns
+names(f.apg) <- c("family", "family.apg", "order")
+#write.csv(f.apg, "./results/apgweb_Spermatophyta_only_checked.csv", row.names = F, quote = F)
+
+
+# NCBI ###################################################################################
+if("NCBI" %in% db){
+  NCBI <- read.csv(paste0(data_folder_path, ncbi_input_filename), header=T, stringsAsFactors = F)
+  
+
+  #Ripogonaceae
+  f.ncbi <- NCBI %>% select(family) %>% unique()
+  fam.apg <-f.apg%>% select(family) %>% unique()
+  
+  setdiff(f.ncbi$family, fam.apg$family)
+  # should be empty.   # [1] "Ripogonaceae" has been acounted for in line 33
+
+  NCBI.tmp <- NCBI %>% 
+    select(-order) %>% 
+    arrange(family) %>% 
+    unique()
+  NCBI.apg <- left_join(NCBI.tmp, f.apg, by="family")
+  saveRDS(NCBI.apg, paste0(data_folder_path, ncbi_output_filename))
+  write.csv(NCBI.apg, "./results/Spermatophyta_NCBI_APG_checked.csv", row.names = F, quote = F)
+  # checklist <- NULL
+  # for (i in NCBI.apg$ncbi_id[duplicated(NCBI.apg$ncbi_id)]){
+  #   dd <- NCBI.apg[NCBI.apg$ncbi_id==i,] %>% select(family, family.apg, order)
+  #   checklist <- rbind.data.frame(checklist, dd)
+  # }
+  # 
+  # checklist <- checklist %>% arrange() %>% unique()  
+}
+
+# WCP ###################################################################################
+if("WCP" %in% db){
+  WCP <- readRDS(paste0(data_folder_path, WCP_input_filename))
+  
+  #check for no match and fix
+  
+  setdiff(unique(WCP$family), unique(f.apg$family))
+  # Should be:  [1] "Byxaceae"       "Gigaspermaceae" "Incertae_sedis"    "Oligomeris"     "Schoberia"      "v" 
+
+  ########################################################################
+  # caution: several invalid strings in fern clade names (check before do)#
+  ########################################################################
+  # #ferns families
+  # "Aspleniaceae"
+  # "Osmundaceae" 
+  # "Polypodiaceae"
+  # "Isoetaceae"
+  # "v"=="Ophioglossum"="1142939-az"
+  # "Ophioglossaceae" 
+  # "Schizaeaceae"
+  # #mosse remove
+  # "Gigaspermaceae"
+  #Incertae_sedis
+  # > unique(WCP[grep("Incertae_sedis", WCP$family),]$genus)
+  # [1] "Angeja"      "Anonymos"    "Cipum"       "Euphrona"    "Ivonia"      "Pouslowia"  
+  # [7] "Theodoricea" "Thuraria"    "Urceola"
+  
+  # remove mosses & incertae_sedis uncomment this line below if want to keep ferns
+  ## Gigaspermatacea: 2 cases, both synonyms (415628-az, 411899-az)
+  ## Invertae_sedis: 20 taxa, all synonyms or unplaced. could be matched as synonym when ignoring the family (step3), so keep them
+  moss.inc <- c("Gigaspermaceae")
+  WCP1 <- WCP %>% 
+    filter(!(family %in% moss.inc)) 
+#    filter(accepted_plant_name_id!="1142939-az") #what`s wrong with this taxon? its an accepted Schizaeaceae fern species`
+  
+  #comment line below if want to keep ferns and uncomment the lines above
+  #ferns.moss.inc <- c("Aspleniaceae", "Osmundaceae", "Polypodiaceae", "Isoetaceae", "Ophioglossaceae", "Schizaeaceae", "Gigaspermaceae", "Incertae_sedis")
+  #WCP1 <- WCP %>% filter(!(family %in% ferns.moss.inc)) %>% filter(accepted_plant_name_id!="1142939-az")
+  
+  #rename
+  # "Oligomeris" "Resedaceae", "1012613-az"
+  # 
+  # "Schoberia" "Amaranthaceae"
+  
+  # > unique(WCP[grep("869344-az", WCP$accepted_plant_name_id),]$family)
+  # [1] "Buxaceae" "Byxaceae"
+  
+  # WCP1$family <- mgsub::mgsub(WCP1$family, c("Byxaceae", "Oligomeris", "Schoberia"), c("Buxaceae", "Resedaceae", "Amaranthaceae"))
+  # correct famlies names
+  WCP1$family <- gsub("Byxaceae", "Buxaceae", WCP1$family)
+  WCP1$family <- gsub("Oligomeris", "Resedaceae", WCP1$family)
+  WCP1$family <- gsub("Schoberia", "Amaranthaceae", WCP1$family)
+  
+  # WCP2 <- WCP1 %>% filter(accepted_plant_name_id!="1142939-az")
+  setdiff(unique(WCP1$family), unique(f.apg$family))
+  
+  # manual fix
+  WCP1$family[WCP1$family=="v"] <- "Ophioglossaceae"
+  
+  WCP.apg <- left_join(WCP1, f.apg, by="family")
+  
+  saveRDS(WCP.apg, paste0(data_folder_path, "apg_", WCP_input_filename))
+  
+}
+
+# BIEN ######################################################################
+if("BIEN" %in% db){
+  bien <- readRDS(paste0(data_folder_path, bien_input_filename))
+  
+  # remove Bryophyta
+  bry <- read.csv(paste0(data_folder_path, bryophyta))
+  bryos <- as.character(bry[,1])
+  bryos <- gsub(" ", "", bryos)
+  bien <- bien[!bien$family %in% bryos,]
+  
+  missing <- setdiff(unique(bien$family), unique(f.apg$family))
+
+  #View(bien[bien$family %in% missing,])
+  # 957 taxa, plenty of green algae
+  library(rgbif)
+  
+  alg.list <- list()
+  for(i in 1:length(missing)){
+    alg.list[[i]] <- rgbif::name_backbone(missing[i])  
+    print(i)
+  }
+
+  phylum <- sapply(alg.list, "[[", "phylum")
+  # fill in missing
+  l <- which(unlist(lapply(phylum, is.null)))
+  phylum[l] <- NA
+  phylum <- unlist(phylum)
+  unique(phylum)
+  # "Rhodophyta" Red algae
+  # "Chlorophyta" Green algae
+  # "Marchantiophyta" liverworts
+  # "Bryophyta" hornworts
+  # "Ascomycota" fungus
+  # "Tracheophyta" vascular plant
+  # "Cyanobacteria" ...
+  # "Basidiomycota" fungus
+  # "Ochrophyta"  yellow-green algae
+  # "Anthocerotophyta" hornworts
+  # "Euglenozoa"  thats not even a plant
+  phylum <- data.frame(missing, phylum)
+  phylum$phylum <- as.character(phylum$phylum)
+  phylum$phylum[is.na(phylum$phylum)] <- "not defined"
+  
+  bien <- left_join(bien, phylum, by=c("family"="missing"))
+  bien$phylum <- as.character(bien$phylum)
+  table(bien$phylum[!is.na(bien$phylum)])
+  
+  # remove all non tracheophyta entries
+  bien <- bien[-which(!is.na(bien$phylum) & bien$phylum!="Tracheophyta"),]
+  
+  bien_sub <- bien[which(bien$phylum=="Tracheophyta"),]
+  nrow(bien_sub)
+  unique(bien_sub$family)
+  # [1] "Plagiogyriaceae"   "Cluisaceae"        "Hemionitidaceae"   "Arthropteridaceae" "Xanthoceraceae"
+  # Plagiogyriaceae is an accepted fern family
+  # Cluisaceae has one genus named Guttiferae which is not on IPNI: remove
+  # Hemionitidaceae, same names as the genus it belongs to, wrong entry: remove
+  # Arthropteridaceae = fern, IPNI, GBIF says synonym of Tectariaceae, keep it like this for now
+  # Xanthoceraceae = typo of Xanthoceratceae (missing t) which is in f.apg
+  bien <- bien[-which(bien$family %in% c("Cluisaceae", "Hemionitidaceae")),]
+  bien$family[which(bien$family=="Xanthoceraceae")] <- "Xanthoceratceae"
+
+  # add Plagiogyriaceae and Arthropteridaceae to the f.apg file 
+  f.apg <- rbind.data.frame(f.apg, c("Plagiogyriaceae", "Plagiogyriaceae", ""),
+                            c("Arthropteridaceae", "Arthropteridaceae", ""))
+  
+  setdiff(unique(bien$family), unique(f.apg$family)) # should be null now
+  
+  bien.apg <- left_join(bien, f.apg, by="family")
+  
+  saveRDS(bien.apg, paste0(data_folder_path, "apg_", bien_input_filename))
+}  
+
+# GBIF #######################################################################################
+if("GBIF" %in% db){
+  gbif <- readRDS(paste0(data_folder_path, gbif_input_filename)) 
+  
+  (sort(fams <- setdiff(unique(gbif$family), unique(f.apg$family))))
+  # manual research
+  # Cyclostigmataceae: source = PBDB, probably doubtful: remove
+  # Hookeriaceae = moss: Elharveya visicularioides
+  # Hymenochaetaceae = fungus, remove
+  # Sphaerocystidaceae = green algea, should not be in the tree: Planochloris pyrenoidifera
+  # Stixaceae = typo of Stixidaceae (included in APG) --> change family name manually here 
+  # Ulotrichaceae = green algae Hormidium ... 181911
+
+  
+  gbif$family <- as.character(gbif$family)
+  gbif$family[which(gbif$family=="Stixaceae")] <- "Stixidaceae"
+  todrop <-  c("Sphaerocystidaceae",
+               "Hookeriaceae",
+               "Cyclostigmataceae", 
+               "Ulotrichaceae",
+               "Hymenochaetaceae")
+  if(length(which(gbif$family %in% todrop))>0){
+    gbif <- gbif[-which(gbif$family %in% todrop),]
+  }
+  
+  setdiff(unique(gbif$family), unique(f.apg$family))
+  # should just be NAs for missing family information
+  
+  gbif.apg <- left_join(gbif, f.apg, by="family")
+  
+  saveRDS(gbif.apg, paste0(data_folder_path, "apg_", gbif_input_filename))
+}
+  
+  
