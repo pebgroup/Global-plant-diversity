c2233c09d15c315d201dd23aadc804e9e5a890c8
Eryops1 <tietje.melanie@gmail.com>
2022-03-13 15:14:41 +0100

    added libraries, added DR calcualtions

--- scripts/10_phylostruct.R
+++ scripts/10_phylostruct.R
@@ -1,21 +1,28 @@
-####################################################
-# 1. Computing RD #
-####################################################
+# Get diversification metrics
+
+wd <- dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(wd)
+rm(list = setdiff(ls(), lsf.str())) 
+library(ape)
+library(castor)
+library(parallel)
+n_cores=4
+
+res <- readRDS("../processed_data/polytomie_RD_results_Sep21_2.rds")
+temp <- readRDS("../processed_data/DR_tip_rates.rds")
+phylo <- read.tree("../processed_data/allmb_matched_added_species_Sep21_clean.tre") 
+comm <- readRDS("../processed_data/comm_September2021.rds")
+
+# compare phylogenetic with geographic data again
+table(colnames(comm) %in% phylo$tip.label) # should be all represented
+
+
+
+# Root distance
 
 dist.all <- get_all_distances_to_root(phylo, as_edge_count=TRUE)
 res$org.rd <- dist.all[1:Ntip(phylo)]
 
-# Correlation of root distance ranges with the former polytomie root distance
-# cor.test(res$range, res$org.rd, method="s")
-# cor.test(res$mean.rd, res$org.rd, method="s")
-# cor.test(res$sd.rd, res$org.rd, method="s")
-# 
-# plot(res$range, res$org.rd, col = alpha("black",0.01))
-# hist(res$range)
-# plot(res$range, res$mean.rd, col = alpha("black",0.01),
-#      xlab="Range RD per species",
-#      ylab="Mean RD")
-# 
 
 
 ####################
@@ -47,3 +54,38 @@ MRD.sd <- unlist(mclapply(1:nrow(comm), mrd.sd, phylo=phylo, RD.sd=RD.sd, mc.cor
 Sys.time()
 
 mrd.res <- data.frame(level3=rownames(comm), mrd=MRD, mrd_sd=MRD.sd)
+saveRDS(mrd.res, "../processed_data/mrd_Sep2021.rds")
+
+
+# Calculate mean DR --------------------------------------------------------
+
+row.names(temp) <- temp$plant_id
+all(row.names(temp) %in% colnames(comm))
+all(colnames(comm) %in% row.names(temp))
+comm <- comm[,-which(! colnames(comm) %in% row.names(temp))]
+# function for parallelization
+av.per.tdwg <- function(i, data, comm.matrix){
+  # data = df with metrics in columns and row names = species ID
+  # returns a named numeric vector
+  presences <- which(row.names(data) %in% colnames(comm.matrix)[comm.matrix[i,]==1])
+  tmp <- apply(data[presences,], 2, mean, na.rm=TRUE)
+  #  tmp <- mean(data[which(data[,colns] %in% data[presences,]),coln])
+  return(tmp)
+}
+
+
+temp <- temp[,-grep("plant_id|ES\\.", names(temp))]
+#rm(res, res.df, sorted_dfl)
+
+n_cores=2
+system.time(
+  test <- mclapply(1:nrow(comm), av.per.tdwg, data=temp,
+                   comm.matrix=comm, mc.cores=n_cores)
+)
+res <- data.frame(
+  MDR = sapply(test, "[[", "MDR"),
+  level3 = row.names(comm))
+
+saveRDS(res, "../processed_data/DR_rates_Feb2022.rds")
+
+
