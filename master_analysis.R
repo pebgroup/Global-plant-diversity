e0c955fa58c815d20df4d2349a7aded5f93aeffa
Eryops1 <tietje.melanie@gmail.com>
2022-03-13 15:11:05 +0100

    moved all libraries to scripts, added descriptions for bash parts, added R package greps

--- master_analysis.R
+++ master_analysis.R
@@ -1,376 +1,198 @@
-# September 28, 2021
-# Written by Melanie Tietje, contributions by Wolf L. Eiserhardt
+# March 2022
+# 
+#
+# Most parts written by Melanie Tietje, contributions by Wolf L. Eiserhardt
 #  
 # 
-# This script sources all analysis scripts in the right order. 
+# This script lists (and sources) all analysis scripts with a brief description.
 # 
-# Please note that some scripts may have a long runtime (hours) and require more
+# Please note that some scripts have a long runtime (hours) and require more
 # computational resources than the average computer (e.g. RAM and multicore
-# parallel processing). To save time, core results of steps requiring a long
-# runtime are provided to allow to repeat following steps without further delay.
-# Scripts with long runtime and/or requiring multiple cores:
-# - add_species_server_version.R
-# - resolve_polytomies.R
-# - get_climate.R
-# - get_soil.R
-# - mod_selection_SEM.R
+# parallel processing). To save time and make results accessible without
+# requiring access to a computer cluster, we provide the results of these steps.
+# It is best to run those on a computational cluster, as some simply require
+# excessive amounts of memory (e.g. TACT: 140GB). Bash scripts included are set
+# up to run on a cluster with Slurm Workload Manager. Scripts with a long
+# runtime are marked as such. Since internal parallelization in R proves
+# difficult on some server setups, parallel processes are performed by cutting
+# data in chunks, starting several processes.
+# 
 # 
 # Scripts 1-18 are data compilation, prep and model selection, for working with
-# the final data see 19_sem_results_further_analysis.R
+# the readily assembled data see 19_sem_results_further_analysis.R
 #
 #
-# Tested with R version 3.6.3, in RStudio Version 1.3.1073
-# 
-# All used R packages are cited in the supplement
+# Tested with R version 4.1.2., RStudio 2022.02.0+443 "Prairie Trillium" Release
+# (9f7969398b90468440a501cf065295d9050bb776, 2022-02-16) for Ubuntu Bionic
 
 
 
-
-# Setup -------------------------------------------------------------------
-
-ncores = 16
-
-dir.create("figures")
-dir.create("processed_data")
-
-source("scripts/0_functions.R")
-library(ape)
-library(caret)
-library(castor)
-library(cowplot) # for multiplot arrangements
-library(data.table)
-library(dplyr)
-library(foreach)
-library(gbm)
-library(geosphere)
-library(ggcorrplot)
-library(ggplot2)
-theme_set(theme_bw())
-library(ggpubr)
-library(ggsci)
-library(lavaan)
-library(parallel)
-library(phytools)
-library(plyr)
-library(png)
-library(raster)
-library(rgdal)
-library(rgeos)
-library(semPlot)
-library(sf)
-library(Simpsons)
-library(spatialEco)
-library(spdep)
-library(stringr)
-library(tidyr)
-library(rgbif)
-library(writexl) # to export model results as table 
+# WCVP data prep #########################################################
 
 
-
-# WCVP PREP ###############################################################
-
-# Process WCVP data -------------------------------------------------------
-# clear workspace, keep functions
-rm(list = setdiff(ls(), lsf.str())) 
-
-wcp <- fread("data/WCVP/world_checklist_names_and_distribution_JUL_21/checklist_names.txt",
-             quote="", na.strings = "")
-saveRDS(wcp, "processed_data/wcp_jul_21.rds")
-
-
-
-# Fix family names following APG IV ---------------------------------------
-db <- "WCP" 
-data_folder_path <- "./processed_data/"
-WCP_input_filename <- "wcp_jul_21.rds"
+# *** Check family names following APG IV ---------------------------------------
 source("1_APG_family_lookup.R")
 
 
-
-# Clean WCVP --------------------------------------------------------------
-# clear workspace, keep functions
-rm(list = setdiff(ls(), lsf.str())) 
-
-wcp <- readRDS("processed_data/apg_wcp_jul_21.rds")
+# *** Clean WCVP --------------------------------------------------------------
 source("2_WCP_cleanup.R")
-saveRDS(wcp2, "processed_data/apg_wcp_jul_21_clean.rds")
-
 
 
 
 
 # PHYLOGENY  ##############################################################
 
-
 ## 1) match tip labels Smith & Brown phylogeny with WCVP names
 ## 2) add missing species taxonomically
-## 3) calculate root distances for all tip labels, then average per TDWG level 3 unit
+## 3) calculate root distances and DR for all tips
+## 4) get verage per TDWG level 3 unit
 
 
-# Match phyologeny tip labels to WCVP, part 1 -----------------------------
-# clear workspace, keep functions
-rm(list = setdiff(ls(), lsf.str())) 
-
-tax_level <- "elevated_to_species_id" 
-
-phylo <- read.tree("data/ALLMB.tre") # phylogeny from Smith&Brown 2018
-ott <- readRDS("data/ott.rds") # OTL taxonomy to identify tip labels
-matches <- readRDS("data/fin_species_match_NCBI_wcvp21.rds") # NCBI-WCSP matches from the taxonomy matcher
-
+# *** Match phylogeny tip labels to WCVP, part 1 -----------------------------
 source("3_match_data.R")
 
-# export tip labels that have not been matched with NCBI
-saveRDS(fin[!is.na(fin$gbif_id),], file="processed_data/SB_tip_labels_21.rds")
-# export all tip labels with source
-saveRDS(fin, file="processed_data/fin.rds")
+
+# *** Prep tip labels for taxonomy matching -----------------------------------
+source("4_common_format_creator_gbif.R")
 
 
 
-# Prep unmatched tip labels for taxonomy matching ------------------------
-# clear workspace, keep functions
-rm(list = setdiff(ls(), lsf.str())) 
-
-# Load Smith & Brown tip labels not covered by NCBI (-->GBIF)
-sb <- readRDS("processed_data/SB_tip_labels_21.rds")
-
-
-# GBIF download to complete tax info --------------------------------------
-rm(list = setdiff(ls(), lsf.str())) 
-sb <- readRDS("SB_tip_labels_21.rds")
-ids <- sb$gbif_id
-res <- list()
-for(i in 1:length(ids)){
-  tryCatch({q
-    res[[i]] <- name_usage(ids[i], return="data")
-    if(!i%%10)cat(i,"\r")
-    # failsafe
-    if(i %in% c(50000, 100000, 150000, 200000)){saveRDS(res, "gbif.rds")}
-    if(i==length(ids)){saveRDS(res, "gbif_SB_tip_labels_21.rds")}
-  }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
-}
-
-
-
-# Prep tip labels for taxonomy matching -----------------------------------
-rm(list = setdiff(ls(), lsf.str())) 
-gbif <- readRDS("data/gbif_SB_tip_labels_21.rds")
-source("4_common_format_creator_SB.R")
-saveRDS(input, "processed_data/input_tip_labels.rds")
-
-
-
-# Fix family names following APG IV ---------------------------------------
-db <- "GBIF" 
-data_folder_path <- "./processed_data/"
-gbif_input_filename <- "input_tip_labels.rds"
+# *** Check family names following APG IV ---------------------------------------
+#!! set the database to GBIF
 source("1_APG_family_lookup.R")
-# writes apg_input_tip_labels.rds
 
 
 
-# Run taxonomy matchig for GBIF tip labels --------------------------------
-# clear workspace, keep functions
-rm(list = setdiff(ls(), lsf.str())) 
+# *** Run taxonomy matching for GBIF tip labels -------------------------------
 
+wd <- dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(wd)
 DB.name <- "GBIF"
-data_folder_path <- "./processed_data/" 
-results_folder_path <- "./processed_data/"
+data_folder_path <- "processed_data/" 
+results_folder_path <- "processed_data/"
 wcvp_input_filename <- "apg_wcp_jul_21_clean.rds"
 gbif_input_filename <- "apg_input_tip_labels.rds"
 fin_file_species <- paste0("fin_species_match_", DB.name, ".rds")
-bry <- read.csv("data/bryophyta.csv")
 
 source("5_taxonomic_matcher.v.1.4.R")
 # output: fin_species_match_GBIF.rds
 
 
-# Match phyologeny tip labels to WCVP, part 2 -----------------------------
-rm(list = setdiff(ls(), lsf.str())) 
-
-matches <- readRDS("processed_data/fin_species_match_GBIF.rds")
-fin <- readRDS("processed_data/fin.rds")
-phylo <- read.tree("data/ALLMB.tre") # phylogeny
-tax_level <- "elevated_to_species_id" 
-phylogb_a <- read.tree("data/GBMB.tre") # genbank tree to identify tips with molecular data
+# *** Match phyologeny tip labels to WCVP, part 2 -----------------------------
 source("6_match_data_part2.R")
-write.tree(clean_tree, "processed_data/allmb_matched_clean.tre")
+# writes allmb_matched_clean.tre
 
 
 
-# Adding species ----------------------------------------------------------
+# *** Adding species ----------------------------------------------------------
 ## LONG RUNTIME ! ca 11 hours
-
-rm(list = setdiff(ls(), lsf.str())) 
-
-phylo <- read.tree("processed_data/allmb_matched_clean.tre")
-wcp <- readRDS("processed_data/apg_wcp_jul_21_clean.rds")
-ferns <- as.vector(read.csv("data/fern_list.txt")[,1])
 
 source("7_add_species_server_version.R", print.eval=T)
 # writes allmb_matched_added_species_Sep21_clean.tre
 
 
 
-# Resolve polytomies, get root distances -----------------------------------
-## LONG RUNTIME ! (ca 1h on 16 cores)
+# *** Resolve polytomys, get diversification measures -------------------------
+## LONG RUNTIME 
 ## PARALLEL PROCESSING
 
-rm(list = setdiff(ls(), lsf.str())) 
-
-
-n_cores =ncores
-reps <- 1000
+## Root Distance  (ca 1h on 16 cores)
 source("8_resolve_polytomies.R", print.eval = TRUE)
-saveRDS(res, "processed_data/polytomie_RD_results.rds")
-# writes polytomie_RD_results_Sep21.rds
 
+
+## prep TACT runs
+source("8.1.1_TACT_and_diversification_rate.R")
+
+## TACT and DR
+# 
+# Submit 50 jobs using the job array bash script
+# "processed_data/tact/tact_job_array.sh". Each of the started jobs needs 145GB
+# memory and has a runtime of 90h. Take care files are all located within the
+# same folder. Required files are 'goodsp.csv' and backbone tree
+# 'gbmb_matched_no_misplaced.tre', as well as a TACT installation (in this case
+# we call a .sif file). If all goes well, you will have 50 TACTed trees. We
+# provide all TACTed trees in the supplement.
+
+## process TACTed trees, get DR
+source("8.1.2_TACT_and_diversification_rate.R")
 
 
 # GEOGRAPHY ##################################################################
 
 
-
-# Build presence matrix ----------------------------------------------------
-# clear workspace, keep functions
-rm(list = setdiff(ls(), lsf.str())) 
-
-
-phylo <- read.tree("processed_data/allmb_matched_added_species_Sep21_clean.tre") 
-wcp <- readRDS("processed_data/apg_wcp_jul_21_clean.rds")
-shape <- readOGR("data/shapefile_bot_countries/level3.shp")
-dist <- fread("data/WCVP/world_checklist_names_and_distribution_JUL_21/checklist_distribution.txt")
+# *** Build presence matrix -------------------------------------------------
 source("9_process_geography.R", print.eval = TRUE)
-saveRDS(comm, file="processed_data/comm_September2021.rds")
 
 
 
-# Mean root distance for each TDGW level 3 unit ----------------------------
-rm(list = setdiff(ls(), lsf.str())) 
-
-
-res <- readRDS("processed_data/polytomie_RD_results_Sep21_2.rds")
-phylo <- read.tree("processed_data/allmb_matched_added_species_Sep21_clean.tre") 
-comm <- readRDS("processed_data/comm_September2021.rds")
-
-# compare phylogenetic with geographic data again
-table(colnames(comm) %in% phylo$tip.label) # should be all represented
-
-n_cores=4
+# *** Mean root distance for each TDGW level 3 unit -------------------------
 source("10_phylostruct.R", print.eval = TRUE)
-saveRDS(mrd.res, "processed_data/mrd_Sep2021.rds")
 
 
 
-
-
-
-# Environmental variables ####################################################
-
+# ENVIRONMENT  ##############################################################
 ## LONG RUNTIME ! ca 6 hours
 
-rm(list = setdiff(ls(), lsf.str())) 
-
-# raster files are loaded inside the scripts
-dir.create("processed_data/cru")
-
+# *** Climate and climate anomalies -------------------------------------------
 source("11_get_climate.R")
-saveRDS(res, "processed_data/climate.rds")
 
 
-rm(list = setdiff(ls(), lsf.str())) 
-
-shape <- readOGR("data/shapefile_bot_countries/level3_mod.shp")
-soil <- raster("data/soil_raster_layer_000832.tif")
+# *** Get number of soil types ------------------------------------------------
 source("12_get_soil.R")
-saveRDS(res, file="processed_data/soil.rds")
 
-rm(list = setdiff(ls(), lsf.str())) 
-shape <- readOGR("data/shapefile_bot_countries/level3_mod.shp")
-elev <- raster("data/wc2.1_30s_elev.tif")
+
+# *** Get terrain ruggedness and elevational range ----------------------------
 source("13_get_topography.R")
-saveRDS(res, file="processed_data/topography.rds")
 
-rm(list = setdiff(ls(), lsf.str()))  
 
-shape <- readOGR("data/shapefile_bot_countries/level3.shp")
-olson <- readOGR("data/shapefile_biomes/wwf_terr_ecos.shp")
+# *** Get biome data ----------------------------------------------------------
 source("14_get_biomes.R")
-saveRDS(res.df, file="processed_data/biomes_olson.rds")
 
 
 
 # Assemble final dataset --------------------------------------------------
-rm(list = setdiff(ls(), lsf.str()))  
-
-sr <- readRDS("processed_data/comm_September2021.rds")
-#sr_2020 <- readRDS("processed_data/comm_April2021.rds")
-mrd <- readRDS("processed_data/mrd_Sep2021.rds")
-#mrd_2020 <- readRDS("processed_data/mrd_Apr2021.rds")
-shape <- readOGR("data/shapefile_bot_countries/level3.shp")
-soil <- readRDS("processed_data/soil.rds")
-climate <- readRDS("processed_data/climate.rds")
-topography <- readRDS("processed_data/topography.rds")
-biomes <- readRDS("processed_data/biomes_olson.rds")
-
 source("15_assemble_dataset.R")
-saveRDS(shp, "processed_data/shp_object_fin_analysis.RDS")
-
 
 
 # Data prep and checks -----------------------------------------------------
-# variable importance, correlations, scaling and variable distributions
-rm(list = setdiff(ls(), lsf.str()))  
+## LONG RUNTIME; PARALLEL PROCESSING (ca 2h)
+# 
+# Parallelized via bash script. Run 'var_selection_master.sh' for 100 GBMs per
+# metric, chose metric in the bash script. Results are being called in the
+# following R script.
 
-shp <- readRDS("processed_data/shp_object_fin_analysis.RDS")
 source("16_data_prep_and_checks.R")
-# saves the final dataset with only complete cases: sem_input_data.rds
-
-
+# saves the final dataset for SEM: sem_input_data.rds
 
 
 
 # STRUCTURAL EQUATION MODELING ###########################################
 
-# SEM model selection -----------------------------------------------------
-## LONG RUNTIME ! ca 5 hours on 16 cores
+# *** SEM model selection -----------------------------------------------------
+## LONG RUNTIME ! ca 1 hour on 32 cores
 ## PARALLEL PROCESSING
+#
+# Parallel processing as achieved externally by dividing the data into 32
+# chunks, each being processed in a different job. Jobs are started running bash
+# script 'mod_selection_master.sh', which calls either
+# 'mod_selection_sequential.sh' or 'mod_selection_sequential_DivRate.sh',
+# calling their respective R scripts.
 
-rm(list = setdiff(ls(), lsf.str()))  
-
-dat_no.na <- readRDS("data/sem_input_data.rds")
-
-# set max number of variables (to add to the fixed ones) used and register cores
-var.max <- 10
-registerDoParallel(ncores)
-
-source("17_mod_selection_SEM.R")
-#save(temp, file="processed_data/mod_selection_Sep2021.RData")
+# run '../processed_data/sem/mod_selection_master.sh'
 
 
 
-# Process SEMs ------------------------------------------------------------
-# get stats from model selection runs and manually adjust models for better fit
-rm(list = setdiff(ls(), lsf.str()))  
+# *** Process SEMs ------------------------------------------------------------ 
+## Get stats from model selection runs and manually adjust models for better fit.
+## Explores models with interaction effects and island vs mainland groups.
 
-load("processed_data/mod_selection_Sep2021.RData")
-dat_no.na <- readRDS("processed_data/sem_input_data.rds")
 source("18_mod_selection_SEM_analysis.R", print.eval = TRUE)
-
-save.image("processed_data/all_models.RData")
-save(max.var.mod_mod7, max.var.mod.fit.mod7, file="processed_data/best_model.RData")
-
-
+source("18.1_mod_selection_DicRate_SEM_analysis.R", print.eval = TRUE)
 
 
 # Maps, Figures, Output --------------------------------------------------
 ## model parameters, local effects, spatial autocorrelation assessment and
 ## correction, latitudinal SR and MRD patterns, robustness
-rm(list = setdiff(ls(), lsf.str()))  
-
-load("processed_data/best_model.RData")
-shp <- readRDS("processed_data/shp_object_fin_analysis.RDS")
-dat_no.na <- readRDS("processed_data/sem_input_data.rds")
 
 source("19_sem_results_further_analysis.R")
 
@@ -378,7 +200,36 @@ source("19_sem_results_further_analysis.R")
 
 
 
+# List R packages & Data --------------------------------------------------
+
+## Called R packages
+p <- system2(command = "grep", args='-r library ../processed_data', stdout=TRUE)
+p2 <- system2(command = "grep", args='-r library ', stdout=TRUE)
+p <- c(p,p2)
+p <- gsub(".*library\\(", "", p)
+p <- gsub("\\)", "", p)
+p <- unique(p)
+p
+
+## R packages dependencies
+pack <- available.packages(fields="Depends")
+pack <- pack[row.names(pack) %in% p,]
+depends <- as.character(pack[,c("Depends")])
+depends <- gsub("R \\(.*?\\)|,", "", depends)
+depends <- strsplit(depends, " ")
+depends <- unique(unlist(depends))
+depends_f <- unique(gsub("[0-9]|\\(.*|\\.|\\)|\\n|-", "", depends))
+depends_f
+
+# loaded data files
+ld <- system2(command = "egrep", args="-r readRDS\\|read\\.csv\\|fread\\|read\\.tree", stdout=TRUE)
+ld <- unique(gsub(".*?\\.\\./processed_data/|.*?\\.\\./data/", "", ld))
+ld <- ld[-grepl("function", ld)] 
+ld <- ld[-which(grepl("^[0-9]", ld))]
+ld <- gsub("\\\"\\)", "", ld)
+ld
+
+
+
 
 # FIN #####
-
-
