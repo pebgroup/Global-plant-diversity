5cfa2edcd47261e552aa863f8ff8ddb8d64b77d6
Eryops1 <tietje.melanie@gmail.com>
2022-03-13 15:16:31 +0100

    added libraries and header, added small canopy check

--- scripts/14_get_biomes.R
+++ scripts/14_get_biomes.R
@@ -3,8 +3,19 @@
 # Biome maps: tropical rainforest following Corlett & Primack (2011), Olsen 2001
 
 
+wd <- dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(wd)
+rm(list = setdiff(ls(), lsf.str())) 
+library(raster)
+library(rgdal)
+library(rgeos)
+
+shape <- readOGR("../data/shapefile_bot_countries/level3.shp")
+olson <- readOGR("../data/shapefile_biomes/wwf_terr_ecos.shp")
+
+
 # check projections
-proj4string(olson) == 
+proj4string(olson)
 proj4string(shape)
 
 
@@ -33,4 +44,45 @@ for(i in 1:nrow(shape@data)){
 Sys.time()
 
 res.df <- data.frame(country=shape$LEVEL_3_CO, res)
+
+saveRDS(res.df, file="../processed_data/biomes_olson.rds")
+
+
+
+# Canopy ------------------------------------------------------------------
+
+#dATA: P. Potapov, X. Li, A. Hernandez-Serna, A. Tyukavina, M.C. Hansen, A.
+#Kommareddy, A. Pickens, S. Turubanova, H. Tang, C. E. Silva, J. Armston, R.
+#Dubayah, J. B. Blair, M. Hofton (2020).
+#https://doi.org/10.1016/j.rse.2020.112165
+# 0-60 Forest canopy height, meters
+# 101 Water
+# 102 Snow/ice
+# 103 No data
+
+can <- raster("../data/canopy_height/combined_tif.tif")
+range(getValues(can))
+val <- 103/255*(getValues(can))
+can <- setValues(can, val)
+val[val>60] <- NA # remove water+ice
+can <- setValues(can, val)
+
+can_height <- c()
+can_range <- c()
+for(i in 1:nrow(shape@data)){
+  shape_sub <- subset(shape, shape$LEVEL_3_CO==shape$LEVEL_3_CO[[i]])
+  can_sub <- crop(can, extent(shape_sub)) # crops to the extent
+  can_sub2 <- mask(can_sub, shape_sub) # masks non rectangular patterns
+  can_height_tmp <- cellStats(can_sub2, "mean", na.rm=TRUE)
+  can_height <- c(can_height, can_height_tmp)
+  can_range_tmp <- cellStats(can_sub2, "range")
+  can_range <- c(can_range, diff(can_range_tmp))
+  if(!i%%1)cat(i,"\r")
+}
+
+hist(can_height)
+hist(can_range)
+
+res <- data.frame(level3=shape$LEVEL_3_CO, can_height, can_range)
+saveRDS(res, file="../processed_data/canopy.rds")
 
